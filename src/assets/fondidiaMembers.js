cfe1cbf7bb07a57b4f39b37c8cccd0fb15376fe0
Fondril <markojovanovic.q@yahoo.com>
2023-06-21 15:18:43 +0200

    members page nearly complete

---
+++ src/assets/fondidiaMembers.js
@@ -0,0 +1,34 @@
+const fondidiaMembers = [
+      {
+            index: 0,
+            name: 'Pera',
+            lastName: 'Zdera',
+            disName: 'Uzi',
+            profilePic: '../../assets/profilePics/Uzi-pofilePic.png',
+            stats: {
+                  stamina: 3,
+                  defence: 3,
+                  strenght: 3,
+                  agility: 3,
+                  charisma: 3,
+                  intelect: 3
+            },
+            about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            skills: [
+                  'Knows yoga',
+                  'Tecno prica rumunski',
+                  'Cooking',
+                  'Elokventan'
+            ],
+            projects: [
+                  {
+                        icon: <iframe title='0' src="https://giphy.com/embed/dvx3Oy3RXKAWQ4ZqJh" className='mmf-iframe' ></iframe>,
+                        projectName: 'Sum cool Game',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                        category: "game",
+                  }
+            ]
+      }
+]
+
+export default fondidiaMembers;
